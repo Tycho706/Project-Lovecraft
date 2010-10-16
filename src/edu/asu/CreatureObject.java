c54c3ec009c794bf8789d36ff965475a1d6ac2c1
Steven <shonda1@asu.edu>
2010-10-16 16:09:09 -0700

    more stuff

--- src/edu/asu/CreatureObject.java
+++ src/edu/asu/CreatureObject.java
@@ -1,11 +1,33 @@
 package edu.asu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class CreatureObject extends MobileObject {
-
+	List<RoomObject> _pathback = new ArrayList<RoomObject>();
+	protected CreatureObject _target;
 	public CreatureObject(Node XML) {
 		super(XML);
 	}
-
+	@Override
+	public boolean initialize(){
+		super.initialize();
+		if(_XMLBlock != null){
+			_path = new ArrayList<RoomObject>();
+			NodeList parsePath = Client.getXMLNodes(_XMLBlock, "Path");
+			if(parsePath != null && parsePath.getLength() > 0)
+				for(int i = 0; i < parsePath.getLength(); i++)
+//					_path.add(RoomObject(parsePath.item(i)));  need to find room objects and assign them  
+			return true;
+		}
+		else
+			return false;
+	}
+//	@Override
+//	protected String think(){
+		
+//	}
 }
--- src/edu/asu/MobileObject.java
+++ src/edu/asu/MobileObject.java
@@ -1,11 +1,19 @@
 package edu.asu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.w3c.dom.Node;
 
 public class MobileObject extends GameObject {
-
+	protected int _pathindex = 0;
+	List<RoomObject> _path = new ArrayList<RoomObject>();
+	
 	public MobileObject(Node XML) {
 		super(XML);
 	}
-
+//	@Override
+//	protected String think(){
+		
+//	}
 }
